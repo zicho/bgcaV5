c944e2370d05d76bafdcfeb8f8c988959647bac4
zicho <martin@datanist.se>
2024-01-03 15:02:32 +0100

    refactor pt2

--- src/lib/components/table/TableItemGame.svelte
+++ src/lib/components/table/TableItemGame.svelte
@@ -20,22 +20,21 @@
 </script>
 
 <div class="flex flex-row space-x-4 my-4">
-	<div class="flex flex-none w-32 align-center items-center">
+	<div class="flex flex-none w-16 lg:w-32 items-center">
 		<img
 			src={game.thumbnailUrl || placeholderImg}
 			alt={!game.thumbnailUrl ? `${game.name} placeholder cover art` : `${game.name} cover art`}
 		/>
 	</div>
 
-	<div class="flex-none w-44 flex items-center">
+	<div class="flex-grow lg:flex-none md:w-36 flex lg:items-center text-sm">
 		<div>
 			<a class="font-bold hover:underline" href="/games/{game.bggId}">{game.name}</a>
-            <div class="text-sm opacity-50">{game.yearPublished}</div>
+			<div class="text-sm opacity-50">{game.yearPublished}</div>
 		</div>
-		
 	</div>
 
-	<div class="flex flex-grow items-center">
+	<div class="lg:flex flex-grow lg:items-center text-sm hidden">
 		{#if game.desc}
 			{game.desc}
 		{:else}
@@ -43,13 +42,23 @@
 		{/if}
 	</div>
 
-	<div class="flex flex-none w-24 items-center">
-		<div class="badge-neutral text-xl p-4 text-center w-16">
+	<div class="flex flex-none w-16 lg:w-24 lg:items-center">
+		<div class="badge-neutral text-xl flex items-center justify-center w-12 h-12 lg:w-16 lg:h-16">
 			{game.averageRating?.substring(0, 3)}
 		</div>
 	</div>
 
-	<div class="flex flex-none w-32 align-center items-center">
-		<LinkButton props={detailsButtonProps} />
+	<div class="flex flex-none w-32 lg:items-center">
+		<LinkButton props={detailsButtonProps} extraClasses="w-full" />
 	</div>
 </div>
+
+<div class="lg:flex flex-grow lg:items-center text-sm lg:hidden">
+	{#if game.desc}
+		{game.desc}
+	{:else}
+		<i class="text-secondary">Description missing</i>
+	{/if}
+</div>
+
+<hr class="mt-4" />
--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -36,7 +36,7 @@
 		<slot slot="headers">
 			<!-- Cover art -->
 			<TableHeader />
-			<TableHeader name="Name" width="w-44" />
+			<TableHeader name="Name" width="w-36" />
 			<TableHeader name="About" fullWidth />
 			<TableHeader name="Rating" width="w-24" />
 			<!-- Link to desc -->
@@ -45,52 +45,6 @@
 		<slot slot="body">
 			{#each games as game}
 				<TableItemGame {game} />
-				<!-- <tr>
-					<td class="pl-0">
-						<div class="flex items-center space-x-3">
-							<div class="avatar">
-								<div class="w-32 h-32">
-									<a href="/games/{game.bggId}">
-										<img
-											src={game.thumbnailUrl || placeholderImg}
-											alt={!game.thumbnailUrl
-												? `${game.name} placeholder cover art`
-												: `${game.name} cover art`}
-										/>
-									</a>
-								</div>
-							</div>
-						</div>
-					</td>
-					<td class="px-0">
-						<div class="flex items-center space-x-3">
-							<div>
-								<div class="font-bold hover:underline min-w-[200px]">
-									<a href="/games/{game.bggId}">{game.name}</a>
-								</div>
-								<div class="text-sm opacity-50">{game.yearPublished}</div>
-							</div>
-						</div>
-					</td>
-					<td class="hidden sm:hidden md:table-cell p-0">
-						<div class="flex">
-							{#if game.desc}
-								{game.desc}
-							{:else}
-								<i class="text-secondary">Description missing</i>
-							{/if}
-						</div>
-					</td>
-
-					<td>
-						<div class="badge-neutral text-xl p-4 text-center">
-							{game.averageRating?.substring(0, 3)}
-						</div>
-					</td>
-					<th class="table-cell px-0">
-						<a href="/games/{game.bggId}" class="btn btn-secondary">details</a>
-					</th>
-				</tr> -->
 			{/each}
 		</slot>
 	</Table>
